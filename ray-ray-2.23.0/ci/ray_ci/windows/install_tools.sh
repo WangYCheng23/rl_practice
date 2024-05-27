9369c02a4d993ff555cc6ffd7a8244fb938a73c3
WangYCheng23 <ucdcheng@ucdavis.edu>
2024-05-27 22:30:01 +0800

    new file:   ray

--- ray-ray-2.23.0/ci/ray_ci/windows/install_tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-powershell ci/pipeline/fix-windows-container-networking.ps1
-pip install awscli
--- ray-ray-2.23.0/python/ray/_version.py
+++
@@ -1,6 +0,0 @@
-# Replaced with the current commit when building the wheels.
-commit = "{{RAY_COMMIT_SHA}}"
-version = "2.23.0"
-
-if __name__ == "__main__":
-    print("%s %s" % (version, commit))
